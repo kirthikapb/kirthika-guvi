70a07eaffe07d6bd83429a4a5cea3049e6a8932f
kirthikapb <94897184+kirthikapb@users.noreply.github.com>
2021-11-25 12:27:07 +0530

    Add files via upload

---
+++ functional component/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Counterhook from './counterhook';
+function App() {
+  return (
+    <div className="App">
+    <Counterhook></Counterhook>
+      </div>  
+  );
+}
+
+export default App;
